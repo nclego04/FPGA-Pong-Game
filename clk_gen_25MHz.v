e0944ca2b0a05f9d7067bea6624dde3e838396ad
Nathan Cinco <56283492+nclego04@users.noreply.github.com>
2026-05-31 19:01:50 -0700

    Create clk_gen_25MHz module for 25MHz clock generation
    Implement a clock generator module for 25MHz output.

---
+++ clk_gen_25MHz.v
@@ -0,0 +1,42 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 05/31/2026 03:36:07 PM
+// Design Name: 
+// Module Name: clk_25MHz
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module clk_gen_25MHz(
+    input clk_100MHz,
+    input reset,
+    output clk_25MHz
+    );
+    
+    reg [1:0] counter;
+    
+    always @(posedge clk_100MHz or posedge reset)
+    begin
+        if (reset)
+            counter <= 0;
+        else
+            counter <= counter + 1;
+            
+    end
+    
+    assign clk_25MHz = counter[1];
+
+endmodule
